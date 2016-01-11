5946966a6bea408048da54721abf68dfb3dac087
dtopalov <dimiter.topalov79@gmail.com>
2016-01-11 21:34:00 +0200

    express hw

---
+++ ExpressHW/server/data/tablets-data.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let tablets = [
+    { manufacturer: 'Acer', model: 'Predator', price: 300},
+    { manufacturer: 'Acer', model: 'One', price: 500},
+    { manufacturer: 'Apple', model: 'Ipad Pro', price: 1000},
+    { manufacturer: 'Apple', model: 'Ipad Mini', price: 500},
+    { manufacturer: 'Microsoft', model: 'Surface Pro 4', price: 1500}
+];
+
+module.exports = tablets;
